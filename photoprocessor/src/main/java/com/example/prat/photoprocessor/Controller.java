026f75def999177551ce3cba9f5e0a9f31c62b21
Prathap Dhanasekaran <prathapprof@gmail.com>
2024-02-18 13:59:33 +0100

    List changed to HashMap and deleteMapping inclusion

--- photoprocessor/src/main/java/com/example/prat/photoprocessor/Controller.java
+++ photoprocessor/src/main/java/com/example/prat/photoprocessor/Controller.java
@@ -1,14 +1,23 @@
 package com.example.prat.photoprocessor;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
-import java.util.List;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 public class Controller {
+     private Map<String, Photo> dataBase = new HashMap<>(){{
+         put("1", new Photo("1","file1.jpg"));
+     }};
 
-    private List<Photo> dataBase = List.of(new Photo("1","file1.jpg"));
     @GetMapping("/")
     public String hello()
     {
@@ -16,8 +25,24 @@ public class Controller {
     }
 
     @GetMapping("/photo")
-    public List<Photo> get()
+    public Collection<Photo> get()
     {
-        return dataBase;
+        return dataBase.values();
     }
+
+    @GetMapping("/photo/{id}")
+    public Photo get(@PathVariable String id)
+    {
+        Photo photo = dataBase.get(id);
+        if(photo == null) throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+        return photo;
+    }
+
+    @DeleteMapping("/photo/{id}")
+    public void delete(@PathVariable String id)
+    {
+        Photo photo = dataBase.remove(id);
+        if(photo == null) throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+         }
+
 }
